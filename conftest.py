8c8c65f4976c0f0c9c83cec3b0deb3e41580dbb6
Vladimir Yankovskiy <vyankovskiy@gmail.com>
2024-06-17 10:45:41 +0500

    add: fixture ingredient

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
 import pytest
 
-from src.bun import Bun
+from src.praktikum.bun import Bun
+from src.praktikum.ingredient import Ingredient
+from src.praktikum.ingredient_types import INGREDIENT_TYPE_FILLING
 
 
 @pytest.fixture
 def bun():
-    name = 'Космоблука'
-    price = 3.99
+    name = 'Флюоресцентная булка R2-D3'
+    price = 989.99
     return Bun(name, price)
+
+
+@pytest.fixture
+def ingredient():
+    ingredient_type = INGREDIENT_TYPE_FILLING
+    name = 'Биокотлета из марсианской Магнолии'
+    price = 424
+    return Ingredient(ingredient_type, name, price)
